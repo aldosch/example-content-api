da5386adfa47f02ac7df8d3fe27b2c967e293824
aldosch <git@aldo.io>
2024-01-25 20:26:10 +1100

    reduce image size

--- src/app/api/content/route.ts
+++ src/app/api/content/route.ts
@@ -6,7 +6,7 @@ const data = {
   description:
     "An apple is a round, edible fruit produced by an apple tree (Malus spp., among them the domestic or orchard apple; Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found. Apples have been grown for thousands of years in Asia and Europe and were introduced to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.",
   image:
-    "https://upload.wikimedia.org/wikipedia/commons/a/a6/Pink_lady_and_cross_section.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Pink_lady_and_cross_section.jpg/640px-Pink_lady_and_cross_section.jpg",
   link: "https://en.wikipedia.org/wiki/Apple",
 };
 
